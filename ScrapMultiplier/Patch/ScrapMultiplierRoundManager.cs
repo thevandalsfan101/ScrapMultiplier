9d1095b97edd388f219a6b450c030a0c2e996c4b
ktkobus <katherine.kobus@gmail.com>
2024-01-25 15:49:58 -0700

    Add project files.

---
+++ ScrapMultiplier/Patch/ScrapMultiplierRoundManager.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace ScrapMultiplier
+{
+    [HarmonyPatch(typeof(RoundManager))]
+    public static class ScrapMultiplierRoundManager
+    {
+        [HarmonyPatch("SpawnScrapInLevel")]
+        [HarmonyPrefix]
+        public static void SpawnScrapInLevelPrefix(RoundManager __instance)
+        {
+            __instance.scrapAmountMultiplier = ScrapMultiplierPlugin.ScrapMultiplierConfig.Value;
+        }
+    }
+}
